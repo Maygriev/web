54aef05ba42b7a777d679a0bce78f05f9c3df81e
Vanilo Alexandre de Medeiros Dantas <vanilo.dantas@academico.ifrn.edu.br>
2019-08-23 21:52:39 -0300

    teste

--- teste/js/artigo.js
+++ teste/js/artigo.js
@@ -1,9 +1,29 @@
 var xhr = new XMLHttpRequest();
+var bt = document.querySelector("#update");
 xhr.open("GET", "var/data.json");
+
 xhr.addEventListener("load", function(){
   var json = JSON.parse(xhr.responseText);
   for (var i = 0; i < json.length; i++) {
     console.log(json[i].title);
   }
 });
+
 xhr.send();
+
+bt.addEventListener("click", function(event){
+  event.preventDefault();
+  var tbody = document.querySelector("tbody");
+  console.log(tbody);
+})
+
+function obterObjeto(objeto){
+    var objeto = {
+        titulo : objeto.titulo,
+        autor : objeto.autor,
+        data : objeto.data,
+        texto : objeto.texto
+      };
+      
+    return objeto;
+}
