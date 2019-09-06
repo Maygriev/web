82c165a5dbad9446895b0433cbadf3014e33ec70
Vanilo Alexandre <vanilo.alexandre@gmail.com>
2019-09-06 15:13:54 -0300

    Adicionando scripts para uso do moment.js

--- lista-get2/js/get.js
+++ lista-get2/js/get.js
@@ -1,9 +1,28 @@
 var urlGET = "https://tads-kitchen.herokuapp.com/posts";
+var test = setInterval(temporizador, 1000);
 
 $("#AtualizaLista").on("click", function(){
   esvaziaLista();
   atualizaDados();
 });
+
+function temporizador() {
+  $.get(
+    urlGET,
+    function(data) {
+      atualizaHora(data);
+    }
+  );
+}
+
+function atualizaHora(data) {
+  moment.locale("pt-br");
+  var hora = $(".hora");
+  for (var i = 0; i < hora.length; i++) {
+    var j = hora.length - i - 1;
+    hora[j].innerHTML = moment(data[i].time, "DDMMYYYY").fromNow();
+  }
+}
 
 function atualizaDados() {
   $.get(
@@ -34,7 +53,7 @@ function criaItem(data) {
 
   final = "<div class='d-flex w-100 justify-content-between'>";
   final += "<h5 class='mb-1'>" + data.title + "</h5>";
-  final += "<small>" + data.time + "</small></div>";
+  final += "<small class = 'hora'>" + moment(data.time, "DDMMYYYY").fromNow() + "</small></div>";
   final += "<p class='mb-1'>" + data.text + "</p>";
   final += "<small>" + data.author + "</small>";
 
