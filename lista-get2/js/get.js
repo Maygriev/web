d8c859e6ed50cfbbe67c17323a791a9ae137b95a
Vanilo Alexandre <vanilo.alexandre@gmail.com>
2019-09-05 21:27:47 -0300

    Lista utilizando ancoras.

--- lista-get2/js/get.js
+++ lista-get2/js/get.js
@@ -1,15 +1,38 @@
 var urlGET = "https://tads-kitchen.herokuapp.com/posts";
 
 $("#AtualizaLista").on("click", function(){
-  checaDados();
+  atualizaDados();
 });
 
-
-function checaDados() {
+function atualizaDados() {
   $.get(
     urlGET,
     function(data) {
-      console.log(data);
+      preencheLista(data);
     }
   );
+}
+
+function preencheLista(data) {
+  var lista = $("#lista")[0];
+  for (var i = 0; i < data.length; i++) {
+    var item = criaItem(data[i]);
+    lista.prepend(item);
+  }
+}
+
+function criaItem(data) {
+  var ancora = document.createElement("a");
+  ancora.classList.add("list-group-item");
+  ancora.classList.add("list-group-item-action")
+  var final;
+
+  final = "<div class='d-flex w-100 justify-content-between'>\n<h5 class='mb-1'>" + data.title + "</h5>\n";
+  final += "<small>" + data.time + "</small>";
+  final += "</div>\n<p class='mb-1'>" + data.text + "</p>\n";
+  final += "<small>" + data.author + "</small>";
+
+  ancora.innerHTML = final;
+
+  return ancora;
 }
